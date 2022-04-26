ca5d0175317d7e99798ed6bb6f544156fbd115f8
ZW <f44966022@gs.ncku.edu.tw>
2022-04-26 17:03:46 +0800

    將bot ID & token放至config

--- app_core.py
+++ app_core.py
@@ -2,12 +2,14 @@ from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
-import os
+import configparser
 
 app = Flask(__name__)
+config = configparser.ConfigParser()
+config.read('config.ini')
 # LINE BOT info
-line_bot_api = LineBotApi('fYBn1Yl8BfeXhVGnAgXkL4rQ24OhjMlfw2XXb59NyIwPEqHmj9uZ5slQ2fiKZ4pXJ4gEWKjry1KXIAjUILimRW37praSR8L1lwsfQBaN7hdZ0x92+aGbhgLp4c8NBFzKflOzsfYWVhDE/TxkcF+yrwdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('43f18d6663f03cfa68eb148698d5cce4')
+line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
+handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -21,16 +23,13 @@ def callback():
     abort(400)
   return 'OK'
 
-# Message event
-@handler.add(MessageEvent)
-def handle_message(event):
-  message_type = event.message.type
-  user_id = event.source.user_id
-  reply_token = event.reply_token
-  message = event.message.text
-  line_bot_api.reply_message(reply_token, TextSendMessage(text = message))
+# TextMessage event
+@handler.add(MessageEvent, message=TextMessage) # message=TextMessage、ImageSendMessage、VideoSendMessage、StickerSendMessage
+def echo(event):
+  line_bot_api.reply_message(
+    event.reply_token,
+    TextSendMessage(text=event.message.text)
+  )
 
 if __name__ == "__main__":
-  # port = int(os.environ.get('PORT', 80))
-  # app.run(host='0.0.0.0', port=port)
   app.run()
