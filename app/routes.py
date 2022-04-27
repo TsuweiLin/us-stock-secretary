f7ca7ffe09b8b8d0450fc679853407dd2ae1972b
ZW <f44966022@gs.ncku.edu.tw>
2022-04-27 14:29:17 +0800

    修正檔案結構

--- app/routes.py
+++ app/routes.py
@@ -6,14 +6,14 @@ from linebot.exceptions import InvalidSignatureError
 # 接收 LINE 的資訊
 @app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers['X-Line-Signature']
+  signature = request.headers['X-Line-Signature']
 
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+  body = request.get_data(as_text=True)
+  app.logger.info("Request body: " + body)
 
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+  try:
+    handler.handle(body, signature)
+  except InvalidSignatureError:
+    abort(400)
 
-    return 'OK'
+  return 'OK'
