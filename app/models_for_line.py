c4fda9cf415ea7daf8fc04d666144539ec7b994d
ZW <f44966022@gs.ncku.edu.tw>
2022-04-27 14:15:51 +0800

    修改檔案架構

---
+++ app/models_for_line.py
@@ -0,0 +1,13 @@
+from app import handler, line_bot_api
+
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+# TextMessage event
+@handler.add(MessageEvent, message=TextMessage) # message=TextMessage、ImageSendMessage、VideoSendMessage、StickerSendMessage
+def echo(event):
+  user_id = event.source.user_id
+  username = line_bot_api.get_profile(event.source.user_id).display_name
+  line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+
+def bot_push_message(to_user_id, text):
+  line_bot_api.push_message(to_user_id, TextSendMessage(text))
--- app_core.py
+++ app_core.py
@@ -1,35 +1,4 @@
-from flask import Flask, request, abort
-from linebot import (LineBotApi, WebhookHandler)
-from linebot.exceptions import (InvalidSignatureError)
-from linebot.models import *
-import configparser
-
-app = Flask(__name__)
-config = configparser.ConfigParser()
-config.read('config.ini')
-# LINE BOT info
-line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
-handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
-
-@app.route("/callback", methods=['POST'])
-def callback():
-  signature = request.headers['X-Line-Signature']
-  body = request.get_data(as_text=True)
-  app.logger.info("Request body: " + body)
-  print(body)
-  try:
-    handler.handle(body, signature)
-  except InvalidSignatureError:
-    abort(400)
-  return 'OK'
-
-# TextMessage event
-@handler.add(MessageEvent, message=TextMessage) # message=TextMessage、ImageSendMessage、VideoSendMessage、StickerSendMessage
-def echo(event):
-  line_bot_api.reply_message(
-    event.reply_token,
-    TextSendMessage(text=event.message.text)
-  )
+from app import app
 
 if __name__ == "__main__":
   app.run()
