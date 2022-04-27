4cabae8344ca8f0fb8cc4f527789868dbf89c1a0
ZW <f44966022@gs.ncku.edu.tw>
2022-04-27 16:17:25 +0800

    整理檔案

--- app/__init__.py
+++ app/__init__.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
-from app import routes, models_for_line, notify
+from app import routes, models_for_line
